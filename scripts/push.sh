9084a829e70b3374d71547f314ee55782705831f
Nic Cope <nic.cope@offerupnow.com>
2017-06-25 16:41:58 -0700

    Fix Docker push script

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,11 +4,11 @@ set -e
 
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 
-docker push negz/queue:$(git rev-parse --short HEAD) .
-docker push negz/queue:latest .
+docker push negz/queue:$(git rev-parse --short HEAD)
+docker push negz/queue:latest
 
-docker push negz/qrest:$(git rev-parse --short HEAD) .
-docker push negz/qrest:latest .
+docker push negz/qrest:$(git rev-parse --short HEAD)
+docker push negz/qrest:latest
 
-docker push negz/qcli:$(git rev-parse --short HEAD) .
-docker push negz/qcli:latest .
+docker push negz/qcli:$(git rev-parse --short HEAD)
+docker push negz/qcli:latest
